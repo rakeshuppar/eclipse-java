46c963216e14fa4e6e442f11740da1721454168e
rakeshuppar <rekheshxworkz@gmail.com>
2023-02-09 07:50:02 +0530

    programs on has association null

---
+++ Association/src/xworkz/com/boot/mainRunner/EngineerRunner.java
@@ -0,0 +1,41 @@
+package xworkz.com.boot.mainRunner;
+
+import xworkz.com.boot.main.Degree;
+import xworkz.com.boot.main.Engineer;
+
+public class EngineerRunner {
+
+	public static void main(String[] args) {
+		
+		Engineer engineer=new Engineer();
+		engineer.initialVariables("navvenKumar", 50000);
+		
+		String[]skill={"leadership","creativity","communication"};
+		engineer.setSkills(skill);
+		
+		
+		Degree degree1=new Degree();
+		Degree degree2=new Degree();
+		
+		degree1.setName("BE");
+		degree1.setDuration(4);
+		degree1.setPursuing(false);
+		degree1.setPercentage(75.0d);
+		degree1.setBranch("civil");
+		
+		degree2.setName("BE");
+		degree2.setDuration(4);
+		degree2.setPursuing(false);
+		degree2.setPercentage(85.0d);
+		degree2.setBranch("mechanical");
+		
+		
+		Degree[] degree= {degree1,degree2};
+		engineer.setDegree(degree);
+		engineer.display();
+		
+		
+		
+	}
+
+	}
---
+++ Association/src/xworkz/com/boot/mainRunner/IsroRunner.java
@@ -0,0 +1,33 @@
+package xworkz.com.boot.mainRunner;
+
+import xworkz.com.boot.main.Isro;
+
+import xworkz.com.boot.main.Scientist;
+
+public class IsroRunner {
+
+	public static void main(String[] args) {
+
+		Isro isro = new Isro();
+		String[] loc= { "Bangalore", "Myasore", "Hasan" };
+		isro.setLocation(loc);
+
+		int[] num= { 14, 15, 16, 17 };
+		isro.setSatelliteNo(num);
+		isro.display();
+
+		Scientist scientist1=new Scientist();
+		Scientist scientist2=new Scientist();
+		
+		
+		scientist1.setName("AbdulKalam");
+		scientist1.setDesignation("Director");
+		
+		scientist2.setName("SatishDhawan");
+		scientist2.setDesignation("Chief");
+		
+		Scientist[] scientists= {scientist1,scientist2};
+				isro.setScientist(scientists);
+		        isro.display();
+}
+}
